5c44dee45eed685962a26eb87d6ad05dacf2c78a
janice <janicejiang.d@gmail.com>
2017-07-26 23:19:29 +0800

    implement meetups#new meetups#create

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,2 +1,22 @@
 class MeetupsController < ApplicationController
+  def index
+    @meetups = Meetup.all
+  end
+
+  def new
+    @meetup = Meetup.new
+  end
+
+  def create
+    @meetup = Meetup.new(meetup_params)
+    @meetup.save
+
+    redirect_to meetups_path
+  end
+
+  private
+
+  def meetup_params
+    params.require(:meetup).permit(:title, :description)
+  end
 end
