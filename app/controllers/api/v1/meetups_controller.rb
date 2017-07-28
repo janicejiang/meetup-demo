4c5886ade702b591d486529dcdfda94f33aaf276
janice <janicejiang.d@gmail.com>
2017-07-28 17:27:01 +0800

    install rspec and implement api login test

--- app/controllers/api/v1/meetups_controller.rb
+++ app/controllers/api/v1/meetups_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::MeetupsController < ApplicationController
+class Api::V1::MeetupsController < Api::V1::BaseController
   before_action :authenticate_user!, only: [:index]
 
   def index
