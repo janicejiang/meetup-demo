9e946833579f773c801ed844dbeda33f94896242
janice <janicejiang.d@gmail.com>
2017-07-28 15:36:18 +0800

    add api base and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   resources :meetups do
     resources :comments
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
 end
